2c512ec5aca9f7a5d7b7cb11f69ebb99fa4b33df
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2022-10-04 02:14:57 +0800

    Create review page, hardcoded for now until API to fetch reviews created

---
+++ frontend/src/components/courseReview.js
@@ -0,0 +1,15 @@
+// const CourseReview = ({ review }) => {
+//     return (
+//       <div className="course-review">
+//         <h1 className="course-code">{course.courseCode}</h1>
+//         <h2>{course.courseTitle}</h2>        
+//         <p>{course.au} <strong>AUs</strong></p>
+//         <hr class="solid"></hr>
+//         <p><strong>Prerequisites: </strong>{course.prerequisites}</p>
+//         <p>{course.description}</p>
+//         {/* need a button here to see the indexes, maybe go new page? */}
+//       </div>
+//     )
+//   }
+  
+//   export default CourseReview
