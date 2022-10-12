4db2ff6cb23de5e46690e84fd0d59c2c1d570545
weepots <92163484+weepots@users.noreply.github.com>
2022-10-13 04:04:58 +0800

    Update navbar logo

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,53 +1,55 @@
-import { Link } from 'react-router-dom'
-import { useLogout } from '../hooks/useLogout'
-import { useAuthContext } from '../hooks/useAuthContext'
-import Lottie from "lottie-react"
-import moonAnimation from "../icons/moon.json"
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+import Lottie from "lottie-react";
+import moonAnimation from "../icons/moon.json";
+import "../styles/navbar.css";
 // import { FiMoon } from "react-icons/fi";
 
 const Navbar = () => {
-  const { logout } = useLogout()
-  const { user } = useAuthContext()
+    const { logout } = useLogout();
+    const { user } = useAuthContext();
 
-  const handleClick = () => {
-    logout()
-  }
+    const handleClick = () => {
+        logout();
+    };
 
-  const defaultOptions = {
-    loop: true,
-    animationData: moonAnimation,
-    renderSettings: {
-      preserveAspectRatio: 'xMidYMid Meet'
-    }
-  }
+    const defaultOptions = {
+        loop: true,
+        animationData: moonAnimation,
+        renderSettings: {
+            preserveAspectRatio: "xMidYMid Meet",
+        },
+    };
 
-  return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          {/* <h1>NTUMoon   <FiMoon /></h1> */}
-          <h1>
-            <div><Lottie animationData={moonAnimation} loop={true} /></div>
-            <div>NTUMoon</div>
-          </h1>
-        </Link>
-        <nav>
-          {user && (
-            <div>
-              <span>{user.username}</span>
-              <button onClick={handleClick}>Log out</button>
+    return (
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1 className="align-content-center mb-0">
+                        <div>
+                            <Lottie animationData={moonAnimation} loop={true} />
+                        </div>
+                        <div className="navbar-logo">NTUMoon</div>
+                    </h1>
+                </Link>
+                <nav>
+                    {user && (
+                        <div>
+                            <span>{user.username}</span>
+                            <button onClick={handleClick}>Log out</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </div>
+                    )}
+                </nav>
             </div>
-          )}
-          {!user && (
-            <div>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </div>
-          )}
-        </nav>
-      </div>
-    </header>
-  )
-}
+        </header>
+    );
+};
 
-export default Navbar
+export default Navbar;
