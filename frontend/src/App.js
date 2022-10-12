957708d93b8bcf5c960d76d162f86cd9d3a02228
weepots <92163484+weepots@users.noreply.github.com>
2022-10-13 04:00:32 +0800

    Add settings page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,58 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { useAuthContext } from './hooks/useAuthContext'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 // pages & components
-import Home from './pages/Home'
-import Review from './pages/Review'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Account from './pages/Account'
-import Navbar from './components/Navbar'
+import Home from "./pages/Home";
+import Review from "./pages/Review";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import Navbar from "./components/Navbar";
+import Settings from "./pages/Settings";
 
 function App() {
-  const { user } = useAuthContext()
+    const { user } = useAuthContext();
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route 
-              path="/" 
-              element={user ? <Home /> : <Navigate to="/login" />} 
-              // element={<Home />} 
-            />
-            <Route 
-              path="/review" 
-              element={user ? <Review /> : <Navigate to="/login" />} 
-              //NOT enabling the user authentication check first because i(yuhao) cannot log in
-              // element={<Review />} 
-            />
-            <Route 
-              path="/login" 
-              element={!user ? <Login /> : <Navigate to="/" />} 
-            />
-            <Route 
-              path="/signup" 
-              element={!user ? <Signup /> : <Navigate to="/" />} 
-            />
-            <Route 
-              path="/account" 
-              element={user ? <Account /> : <Navigate to="/" />} 
-            />
-          </Routes>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <div className="pages">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={user ? <Home /> : <Navigate to="/login" />}
+                            // element={<Home />}
+                        />
+                        <Route
+                            path="/review"
+                            element={
+                                user ? <Review /> : <Navigate to="/login" />
+                            }
+                            //NOT enabling the user authentication check first because i(yuhao) cannot log in
+                            // element={<Review />}
+                        />
+                        <Route
+                            path="/login"
+                            element={!user ? <Login /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/signup"
+                            element={!user ? <Signup /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/account"
+                            element={user ? <Account /> : <Navigate to="/" />}
+                        />
+                        <Route
+                            path="/settings"
+                            element={user ? <Settings /> : <Navigate to="/" />}
+                        />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
-      </BrowserRouter>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/Settings.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../hooks/useAuthContext";
+import SideNavBar from "../components/sideNavBar";
+import "../styles/settings.css";
+
+const contentStyle = {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+};
+
+const Settings = () => {
+    const [oldPassword, checkOldPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [cfmPassword, checkCfmPassword] = useState("");
+    const { user } = useAuthContext();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!newPassword) {
+            alert("Please add a password");
+            return;
+        } else if (!cfmPassword) {
+            alert("Please confirm password");
+            return;
+        } else if (newPassword == cfmPassword) {
+            alert("Passwords dont match");
+            return;
+        }
+    };
+
+    return (
+        <div className="settings">
+            <SideNavBar></SideNavBar>
+
+            <div className="settings-cont col-10 form">
+                <div className="settings-title">Settings</div>
+                {/* <div className="form form-control">Enter password here</div> */}
+                <div className="settings-description">
+                    Please Enter your current password to change your password.
+                </div>
+                <div className="settings-line"></div>
+
+                <div className="settings-fields">
+                    <label className="col-3">Current Password: </label>
+                    <input
+                        className="col-4"
+                        type="text"
+                        placeholder="Enter password here"
+                        value={oldPassword}
+                        onChange={(e) => checkOldPassword(e.target.value)}
+                    />
+                </div>
+                <div className="settings-line"></div>
+                <div className="settings-fields">
+                    <label className="col-3">New Password: </label>
+                    <input
+                        className="col-4"
+                        type="text"
+                        placeholder="Enter password here"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                </div>
+                <div className="settings-line"></div>
+
+                <div className="settings-fields">
+                    <label className="col-3">Confirm Password: </label>
+                    <input
+                        className="col-4"
+                        type="text"
+                        placeholder="Enter password here"
+                        value={cfmPassword}
+                        onChange={(e) => checkCfmPassword(e.target.value)}
+                    />
+                </div>
+                <div className="settings-line"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Settings;
