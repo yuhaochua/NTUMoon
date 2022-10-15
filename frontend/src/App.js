1af95f57cda7186d13f8e7376e68c595d7458e33
himreaowi <himariang@gmail.com>
2022-10-15 11:17:37 +0800

    Add Carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,7 +39,7 @@ function App() {
             />
             <Route 
               path="/account" 
-              element={user ? <Account /> : <Navigate to="/" />} 
+              element={user ? <Account /> : <Navigate to="/account" />} 
             />
           </Routes>
         </div>
