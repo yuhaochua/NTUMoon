c7f668f1159bd2c0884f11427655bebaaa8274aa
qinxxxm <soldiero3of@gmail.com>
2022-10-15 23:19:04 +0800

    added getUserCourses

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -9,6 +9,44 @@ const getAllCourses = async (req, res) => {
     res.status(200).json(courses) //send back (response) as json
 }
 
+//get all courses for a specific user
+const getUserCourses = async (req, res) => {
+    try {
+        const {_id} = req.user
+        let listUserCourses = await UserCourse.findOne({user_id: _id})
+        if (!listUserCourses) {
+            throw Error ("No Courses Added")
+        }
+        var listUserCourseInfo = [];
+        // listUserCourses.courses is [{courseCode,index},{},{}]
+        for (i=0; i<(listUserCourses.courses).length; i++) { // userCourse = {courseCode, index}
+            try{
+                const {courseCode, index} = listUserCourses.courses[i]
+                const course = await Course.findOne({courseCode: courseCode})
+                let courseDetails = course.indexes.find(data => data.index == index).details
+                var userCourseInfo = {
+                    courseCode: courseCode,
+                    courseTitle: course.courseTitle,
+                    prerequisites: course.prerequisites,
+                    au: course.au,
+                    description: course.description,
+                    index: index,
+                    details: courseDetails
+                }
+
+                // add to listUserCourseInfo
+                listUserCourseInfo.push(userCourseInfo)
+            }
+            catch(error) {
+                res.status(400).json({error: error.message})
+            }
+        }
+        res.status(200).json(listUserCourseInfo) //send back (response) as json
+    } catch(error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
 //add course for user (takes in courseCode and index from req.body), when calling api from react need to include authorisation...
 const addCourse = async(req, res) => { 
     try {
@@ -61,6 +99,7 @@ const deleteCourse = async(req, res) => {
 
 module.exports = {
     getAllCourses,
+    getUserCourses,
     addCourse,
     deleteCourse
 }
